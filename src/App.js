b92fa16fb0416730d80815753908767de24be046
jaitone <jaitoone@gmail.com>
2021-01-04 14:05:52 +0000

    Added router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-function App() {
-  return (
-    <div className="App">
+import React, { Component } from "react";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            {/* <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/userpage" component={UserPage} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/logout" component={LogOut} />
+            <Redirect exact path="/" to="/home" />
+            <Route path="/*" component={NotFound} /> */}
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
