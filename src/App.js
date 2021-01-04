09408c2fe6a3832d43a23e17a25f3fe6aebd7657
jaitone <jaitoone@gmail.com>
2021-01-04 14:38:19 +0000

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import NavigationBar from "./Components/navigationBar";
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavigationBar/>
         <BrowserRouter>
           <Switch>
             {/* <Route exact path="/signup" component={SignUp} />
---
+++ src/Components/navigationBar.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+const NavigationBar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar className="navbar-dark" color="dark" light expand="md">
+        <NavbarBrand href="/">Welcome!</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink>About me</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>Projects</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>Experience</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>Contact</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavigationBar;
