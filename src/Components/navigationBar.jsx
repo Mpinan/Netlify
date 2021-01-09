d9dfbc4bdd286d510fda8df57808d6999ddb3a37
jaitone <jaitoone@gmail.com>
2021-01-09 18:37:17 +0000

    Added a responsive navbar

--- src/Components/navigationBar.jsx
+++ src/Components/navigationBar.jsx
@@ -1,25 +1,32 @@
 import { FaBars } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 const NavigationBar = (props) => {
+
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
+
   return (
     <div>
       <header>
 				<div className="logo">
 					<a href="/#">Welcome!</a>
 				</div>
-				<nav className="nav">
-				  	<a href="/#">home</a> 
-            <a href="/#">about</a> 
-            <a href="/#">experience</a> 
-            <a href="/#">projects</a> 
-            <a href="/#">contact</a>
-            {/* <--------------> */}
-
-  {/* <---------------> */}
-          <div className="icon">
-              <FaBars />
+          <div className="icon" onClick={toggleNavbar} >
+              <FaBars onClick={toggleNavbar} />
           </div>
-				</nav>
+          <Collapse isOpen={!collapsed} navbar>
+				    <nav className="nav">
+				      	<a href="/#">home</a> 
+                <a href="/#">about</a> 
+                <a href="/#">experience</a> 
+                <a href="/#">projects</a> 
+                <a href="/#">contact</a>
+				    </nav>
+          </Collapse>
 		  </header>
     </div>
   );
