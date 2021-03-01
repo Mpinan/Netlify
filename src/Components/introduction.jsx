f285fd65e8fe632929f5e5140eb0341f49f43a25
jaitone <jaitoone@gmail.com>
2021-03-01 13:37:18 +0000

    fixed phone responsive

--- src/Components/introduction.jsx
+++ src/Components/introduction.jsx
@@ -1,43 +1,43 @@
 const Introduction = () => {
 
-    return (
+	return (
 
-        <div id="home" className="landingpage">
-            <div className="banner-text">
-                <h1>
-                    Looking for a web developer?
+		<div id="home" className="landingpage">
+			<div className="banner-text">
+				<h1>
+					Looking for a web developer?
                     <br />
-                </h1>
-                <h2>
-                    Hi, I'm Mario, a full-stack software developer.
+				</h1>
+				<h2>
+					Hi, I'm Mario, a full-stack software developer.
                 <br />
                     Keep scrolling to see what I do!
                 </h2>
-                <br />
-                <div className="banner-text-col">
-                    <p>
-                        Born and raised in Spain, I have always been a passionate about technology, specially hardware.
+				<br />
+				<div className="banner-text-col">
+					<p>
+						Born and raised in Spain, I have always been a passionate about technology, specially hardware.
                         <br />
-                        <br />
+						<br />
                         But it wasn't untill I came to London, and after a few years of learning the language, that I discovered coding.
                         <br />
-                        <br />
+						<br />
                         Now, I can't stop!
                     </p>
-                </div>
-                <div className="banner-text-col">
-                    <p>
-                        I aim to grow as a software developer, with a view to participating in company projects for the longer term.
+				</div>
+				<div className="banner-text-col">
+					<p>
+						I aim to grow as a software developer, with a view to participating in company projects for the longer term.
                         <br />
-                        <br />
+						<br />
                         I am seeking an opportunity to begin a career whilst furthering my knowledge of computer programming.
                     </p>
-                </div>
-            </div>
+				</div>
+			</div>
 
-        </div>
+		</div>
 
-    );
+	);
 };
 
 export default Introduction;
